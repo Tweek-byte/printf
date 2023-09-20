31c59a707fd07fb654bce665060e24628c36013c
TTO | TweekZ0 <67276721+Tweek-byte@users.noreply.github.com>
2023-09-20 18:55:15 +0100

    Yea... this is hopeless

--- print_functions.c
+++ print_functions.c
@@ -46,67 +46,45 @@ int print_int(va_list ap, params_t *params)
 }
 
 /**
- * print_string - prints a string with the specified precision
- * @ap: the va_list containing the string argument
+ * print_string - prints string
+ * @ap: argument pointer
  * @params: the parameters struct
  *
- * Return: the number of characters printed
+ * Return: number chars printed
  */
 
 int print_string(va_list ap, params_t *params)
 {
-	char *str = va_arg(ap, char *);
-	char pad_char = ' ';
+	char *str = va_arg(ap, char *), pad_char = ' ';
 	unsigned int pad = 0, sum = 0, i = 0, j;
 
 	(void)params;
-	switch (!str)
-	{
+	switch ((int)(!str))
 		case 1:
 			str = NULL_STRING;
-	}
 
 	j = pad = _strlen(str);
 	if (params->precision < pad)
-	{
 		j = pad = params->precision;
-	}
 
 	if (params->minus_flag)
 	{
 		if (params->precision != UINT_MAX)
-		{
-			for (i = 0; i < j; i++)
-			{
+			for (i = 0; i < pad; i++)
 				sum += _putchar(*str++);
-			}
-		}
 		else
-		{
 			sum += _puts(str);
-		}
 	}
-
 	while (j++ < params->width)
-	{
 		sum += _putchar(pad_char);
-	}
-
 	if (!params->minus_flag)
 	{
 		if (params->precision != UINT_MAX)
-		{
 			for (i = 0; i < pad; i++)
-			{
 				sum += _putchar(*str++);
-			}
-		}
 		else
-		{
 			sum += _puts(str);
-		}
 	}
-
 	return (sum);
 }
 
